52837273d9358fc8de83fb30b9024dc835f435f7
teobler <teobler@163.com>
2020-11-03 16:12:14 +0800

    init

---
+++ vue-demo/src/set-public-path.js
@@ -0,0 +1,3 @@
+import { setPublicPath } from "systemjs-webpack-interop";
+
+setPublicPath("@demo/vue-demo", 2);
---
+++ vue-demo2/src/set-public-path.js
@@ -0,0 +1,3 @@
+import { setPublicPath } from "systemjs-webpack-interop";
+
+setPublicPath("@demo/vue-demo2", 2);
