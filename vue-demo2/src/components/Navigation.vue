f2ecd2bff1d26a84379d5ee970452659210b8f37
teobler <teobler@163.com>
2020-11-04 15:55:16 +0800

    modify styling to monitor navgation app with two more apps

---
+++ vue-demo2/src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+    <a href="/react">react</a>
+    <a href="/vue">vue</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  nav {
+    display: block;
+    padding: 15px;
+    height: 100vh;
+  }
+
+  a {
+    display: block;
+    margin-bottom: 5px;
+  }
+</style>
