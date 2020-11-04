c1055b7cce8b206534acc4ed6276e7191a6e8980
teobler <teobler@163.com>
2020-11-04 11:51:15 +0800

    set default url for whole app.

--- react-demo/src/root.component.tsx
+++ react-demo/src/root.component.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Root(props) {
+  // this is a workaround for setting default url
+  // ref: https://github.com/single-spa/single-spa-layout/issues/73
+  useEffect(() => {
+    history.replaceState(history.state, document.title, "/react");
+  }, []);
+
   return <section>{props.name} is mounted!</section>;
 }
